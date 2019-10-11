850f9dfda8d6c92c69963f10d87c987d577652c6
Phils Garcia <eivanphils@gmail.com>
2019-10-11 12:46:23 -0300

    agregado componente nativo para usar barcode

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  slideOpt = {
+    allowSlideNext: false,
+    allowSlidePrev: false
+  };
 
-  constructor() {}
+  constructor(private barcodeScanner: BarcodeScanner ) {}
 
+  scanCode() {
+    this.barcodeScanner.scan().then (barcodeData => {
+      console.log('barcodedata', barcodeData);
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
 }
